838d61e3990b763c6905a2e7ecc3ead384167476
Ryan Freeman <info@ryanfreeman.io>
2019-12-21 22:42:06 +0000

    Updated MainActivity to display photos in a StaggeredGrid RecyclerView

--- app/src/main/java/com/example/photos/MainActivity.java
+++ app/src/main/java/com/example/photos/MainActivity.java
@@ -1,14 +1,55 @@
 package com.example.photos;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.example.photos.model.PhotoEntity;
+import com.example.photos.ui.PhotosAdapter;
+import com.example.photos.utilities.SampleData;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.recycler_view)
+    RecyclerView mRecyclerView;
+
+    private List<PhotoEntity> photosData = new ArrayList<>();
+    private PhotosAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+        initRecyclerView();
+
+        // load photosData ArrayList with photos from SampleData
+        photosData.addAll(SampleData.getPhotos());
+
+        // output each PhotoEntity to Logcat
+        for (PhotoEntity photo :
+                photosData) {
+            Log.i("Photos", photo.toString());
+        }
+    }
+
+    private void initRecyclerView() {
+        StaggeredGridLayoutManager layoutManager =
+                new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
+        mAdapter = new PhotosAdapter(photosData, this);
+
+        mRecyclerView.setLayoutManager(layoutManager);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setAdapter(mAdapter);
     }
 }
