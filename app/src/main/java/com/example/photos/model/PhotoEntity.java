425dca935f5419ee6c02621f424fefaa89280d6b
Ryan Freeman <info@ryanfreeman.io>
2020-01-12 18:47:48 +0000

    Defined Room entity class on PhotoEntity POJO

--- app/src/main/java/com/example/photos/model/PhotoEntity.java
+++ app/src/main/java/com/example/photos/model/PhotoEntity.java
@@ -1,14 +1,27 @@
 package com.example.photos.model;
 
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+
+// define the table name for Room
+@Entity(tableName = "photos")
 public class PhotoEntity {
+
+    // define the primary key for Room
+    @PrimaryKey(autoGenerate = true)
     private int id;
+
     private String title;
     private String thumbnail;
     private String fileName;
 
+    // tell Room to ignore this constructor
+    @Ignore
     public PhotoEntity() {
     }
 
+    // Room will use this constructor
     public PhotoEntity(int id, String title, String thumbnail, String fileName) {
         this.id = id;
         this.title = title;
@@ -16,6 +29,8 @@ public class PhotoEntity {
         this.fileName = fileName;
     }
 
+    // tell Room to ignore this constructor
+    @Ignore
     public PhotoEntity(String title, String thumbnail, String fileName) {
         this.title = title;
         this.thumbnail = thumbnail;
