68f657f94206ae5c1b8d39e214e5e75e6d8b1862
Ryan Freeman <info@ryanfreeman.io>
2020-01-12 17:56:10 +0000

    Removed default click handler for fab button

--- app/src/main/java/com/example/photos/PhotoActivity.java
+++ app/src/main/java/com/example/photos/PhotoActivity.java
@@ -21,15 +21,6 @@ public class PhotoActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         Intent intent = getIntent();
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 }
